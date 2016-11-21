ba8949c879e5cca1f58c29a9f64e0935a2284945
Hui Jun <huijun@Yams-MacBook-Pro.local>
2016-11-21 07:33:56 -0800

    Add W2D1 homework

---
+++ W2D1/skeleton/lib/simon.rb
@@ -0,0 +1,66 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until game_over
+      take_turn
+    end
+
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+
+    unless game_over
+      self.sequence_length += 1
+      round_success_message
+    end
+  end
+
+  def show_sequence
+    add_random_color
+    seq.each do |color|
+      puts color
+      sleep(0.5)
+      system("clear")
+    end
+  end
+
+  def require_sequence
+    put "Please insert the previous color sequence"
+    seq.each do |color|
+      input_color = gets.chomp
+      self.game_over = true if color != input_color
+      break
+    end
+  end
+
+  def add_random_color
+    seq << COLORS.sample
+  end
+
+  def round_success_message
+    "You succeed this round"
+  end
+
+  def game_over_message
+    "You lose"
+  end
+
+  def reset_game
+    self.sequence_length = 1
+    self.game_over = false
+    self.seq = []
+  end
+end

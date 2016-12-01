4eef9a5af1107f2e7a52e58ff2fa819af1221e5c
Hui Jun <huijun@Yams-MacBook-Pro.local>
2016-12-01 03:25:58 -0800

    MOdified queries.rb in active record warm up folder

---
+++ W3D3/Person/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# person1 = Person.create(name: "Hui Jun", house_id: 1)
+# person2 = Person.create(name: "John", house_id: 2)
+# person3 = Person.create(name: "Sarah", house_id: 1)
+
+house1 = House.create!(address: "Howard St")
+house2 = House.create!(address: "Market St")

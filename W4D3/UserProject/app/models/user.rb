1264e15e3e78cbe08bb00afbcd9f63776622c5a6
Hui Jun <huijun@Yams-MacBook-Pro.local>
2016-12-07 12:37:44 -0800

    Update typo

--- W4D3/UserProject/app/models/user.rb
+++ W4D3/UserProject/app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
-    if user && BCrypt::Password.new(password_digest).is_password?(password)
+    if user && BCrypt::Password.new(user.password_digest).is_password?(password)
       return user
     end
   end
